c27d2b1cc138b02c9ba4347dba8406c9323a0de1
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-24 10:10:24 -0300

    construindo o primeiro programa em python

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,1 +1,9 @@
 print("aula 01")
+# Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+
+print(input("Digite o seu nome:"))
+
+#Criar um programa onde o usuário digite dois valores e apareça a soma
+
+print(int(input("Digite o primeiro numero:")) + int(int(input("Digite o segundo numero:"))))
+
